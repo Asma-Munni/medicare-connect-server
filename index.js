2db21edd98a95220f251acede3891afa1dd18f69
Asma-Munni <asmahossain529@gmail.com>
2026-06-25 18:45:59 +0600

    payment route updated

--- index.js
+++ index.js
@@ -40,10 +40,10 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
+// async function run() {
+ // try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+  //  await client.connect();
 
 
 
@@ -1021,8 +1021,8 @@ app.post("/create-payment-session", async (req, res) => {
         patientId: appointment.patientId,
         doctorId: appointment.doctorId,
       },
-      success_url: `${process.env.CLIENT_URL}/dashboard/patient/payment-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${process.env.CLIENT_URL}/dashboard/patient/appointments`,
+      success_url: `${process.env.CLIENT_URL}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.CLIENT_URL}/dashboard/patient/appointments`,
     });
 
     res.send({
@@ -1694,16 +1694,22 @@ app.get("/stats",verifyToken, verifyAdmin, async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+  //  await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+  //} finally {
     // Ensures that the client will close when you finish/error
   //  await client.close();
-  }
-}
-run().catch(console.dir);
+ // }
+//}
+//run().catch(console.dir);
+client.connect(()=>{
+  console.log('connecting to mongodb')
+}).catch(console.dir)
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+module.exports = app;
